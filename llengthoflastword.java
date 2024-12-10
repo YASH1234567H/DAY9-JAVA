de40a2660a0cb469b13e07013da1d0be7e1f5655
Yashwant <anyname6281@gmail.com>
2024-12-10 11:24:57 +0530

    Add files via upload

---
+++ llengthoflastword.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        String s1[]=s.split(" ");
+        int d=s1.length;
+        String h=s1[d-1];
+        return h.length();
+    }
+}
